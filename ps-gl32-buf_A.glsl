a5c8f6d2007c64757a40731bdd9a246140428918
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2022-05-14 10:38:18 -0400

    idk

--- ps-gl32-buf_A.glsl
+++ ps-gl32-buf_A.glsl
@@ -16,6 +16,44 @@ float sdBox( in vec2 p, in vec2 b )
     return length(max(d,0.0)) + min(max(d.x,d.y),0.0);
 }
 
+void cubemap(vec3 r, out float texId, out vec2 st) {
+   vec3 uvw;
+   vec3 absr = abs(r);
+   if (absr.x > absr.y && absr.x > absr.z) {
+     // x major
+     float negx = step(r.x, 0.0);
+     uvw = vec3(r.zy, absr.x) * vec3(mix(-1.0, 1.0, negx), -1, 1);
+     texId = negx;
+   } else if (absr.y > absr.z) {
+     // y major
+     float negy = step(r.y, 0.0);
+     uvw = vec3(r.xz, absr.y) * vec3(1.0, mix(1.0, -1.0, negy), 1.0);
+     texId = 2.0 + negy;
+   } else {
+     // z major
+     float negz = step(r.z, 0.0);
+     uvw = vec3(r.xy, absr.z) * vec3(mix(1.0, -1.0, negz), -1, 1);
+     texId = 4.0 + negz;
+   }
+   st = vec2(uvw.xy / uvw.z + 1.) * .5;
+}
+
+vec3 rayDirection(float fieldOfView, vec2 size, vec2 fragCoord) {
+    vec2 xy = fragCoord - size / 2.0;
+    float z = size.y / tan(radians(fieldOfView) / 2.0);
+    return normalize(vec3(xy, -z));
+}
+
+mat3 viewMatrix(vec3 eye, vec3 center, vec3 up) {
+    // Based on gluLookAt man page
+    vec3 f = normalize(center - eye);
+    vec3 s = normalize(cross(f, up));
+    vec3 u = cross(s, f);
+    return mat3(s, u, -f);
+}
+
+#define PI 3.14159
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     
@@ -30,6 +68,28 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     fragColor = vec4(1);
     return;
     }
+	
+	vec3 viewDir = rayDirection(120.0, iResolution.xy, fragCoord);
+	vec3 eye = vec3(8.0, 5.0 * sin(0.2 * iTime), 7.0);
+
+    float mx=iMouse.x/iResolution.x*PI*2.0;
+    float my=iMouse.y/iResolution.y*PI + PI/2.0;
+    eye = vec3(cos(my)*cos(mx),sin(my),cos(my)*sin(mx));//*7.;
+	
+	mat3 viewToWorld = viewMatrix(eye, vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0));
+	
+	vec3 worldDir = viewToWorld * viewDir;
+
+	float texId;
+	vec2 st;
+	
+	cubemap(worldDir, texId, st);
+	
+	// fragColor = vec4(st.x, st.y, 0, 1);
+	
+	
+    float select = step(float(0) - 0.5, texId) * 
+                   step(texId, float(0) + .5);
 
 	//fragColor = vec4(0);
     //fragColor = vec4(0,pressure,0,0);
@@ -38,7 +98,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     //fragColor = vec4(dd);
     
     
-    if (iMouse.z > 0.0) {
+    if (iMouse.z > 0.0 && select == 1.) {
         vec2 uv2 = (fragCoord - iMouse.xy) / iResolution.y;
         
 
@@ -62,6 +122,9 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 			// fragColor = vec4(1,1,1,1);
 		fragColor = mix(fragColor, vec4(0,.5,1,1), smoothstep(.01,-.0, d));
     }
+	
+	// if(select == 1.)
+		// fragColor = vec4(0, .5, 1, 1);
 }
 
 void main( void ){vec4 color = vec4(0.0,0.0,0.0,0.0);mainImage( color, gl_FragCoord.xy );FragColor = color;}
